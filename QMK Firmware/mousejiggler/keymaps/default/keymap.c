5a80e30c236da6905926f901dc533bbefb3dfb0c
DIYCharles <mcahneriii@gmail.com>
2021-05-21 01:09:44 -0500

    yeet

--- QMK Firmware/mousejiggler/keymaps/default/keymap.c
+++ QMK Firmware/mousejiggler/keymaps/default/keymap.c
@@ -5,6 +5,7 @@ enum custom_keycodes {
   RAPIDF
 };
 
+
 bool rapid_fire = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -19,18 +20,16 @@ void matrix_init_user(void) {
 
 void matrix_scan_user(void) {
   if (rapid_fire) {
-    /* SEND_STRING("yeetyeetyeet");
-    SEND_STRING("yaughtyaughtyaught");*/
-    tap_code(KC_MS_BTN1);
-    tap_code(KC_MS_BTN1);
-    tap_code(KC_MS_BTN1);
+    tap_code(KC_MS_UP);
+    tap_code(KC_MS_DOWN);
+    tap_code(KC_MS_LEFT);
+    tap_code(KC_MS_RIGHT);
+    tap_code(KC_MS_WH_UP);
+    tap_code(KC_MS_WH_DOWN);
   } else {
 
   }
 }
-
-
-
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -45,3 +44,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
+
+    /*    SEND_STRING("1");
+    tap_code(KC_MS_BTN1);
+    tap_code(KC_MS_BTN1);
+    SEND_STRING(SS_LCTL("c"));
+    tap_code(KC_RIGHT);
+    SEND_STRING(SS_LCTL("v"));
+    SEND_STRING("yaughtyaughtyaught");
+    tap_code(KC_MS_BTN1);
+    tap_code(KC_MS_BTN1);
+    tap_code(KC_MS_BTN1);*/
